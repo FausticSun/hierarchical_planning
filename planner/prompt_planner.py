7ae8de81534ad1339a4ef8d84d0ef61e2b0b5923
Wenhan Luo <FausticSun@gmail.com>
2025-09-24 16:48:45 +0800

    add code for agent trajectories

--- planner/prompt_planner.py
+++ planner/prompt_planner.py
@@ -1,5 +1,5 @@
 import os
-from typing import Dict, Tuple
+from typing import Dict, List, Tuple
 
 from langchain_core.language_models.chat_models import BaseChatModel
 from langchain_prompty import create_chat_prompt
@@ -16,7 +16,7 @@ class PromptPlanner(BasePlanner):
     number_of_agents: int = -1
     grid_size: int = -1
     number_of_targets: int = -1
-    agent_positions: Dict[int, Tuple[int, int]]
+    agent_trajectories: Dict[int, List[Tuple[int, int]]]
     found_targets = set()
 
     def __init__(
@@ -30,7 +30,7 @@ class PromptPlanner(BasePlanner):
         self.mission_statement = str(observations[0]["mission"])
         self.number_of_agents = len(observations.keys()) - 1
         self.number_of_targets = observations["global"]["num_goals"]
-        self.agent_positions = {i: (1, 1) for i in range(0, self.number_of_agents)}
+        self.agent_trajectories = {i: [(1, 1)] for i in range(0, self.number_of_agents)}
         self.grid_size = grid_size
         self.tracker = Tracker(grid_size)
 
@@ -55,9 +55,27 @@ class PromptPlanner(BasePlanner):
         self, agents, observations, rewards, terminations, truncations, infos
     ) -> dict:
         del observations["global"]
-        if any([r == 1 for r in rewards.values()]) or any(
-            agents.idle(i) for i in range(self.number_of_agents)
+        for k, v in observations.items():
+            location = tuple(int(x) for x in v["location"])
+            self.agent_trajectories[k].append(location)
+        stuck = False
+        for k, v in self.agent_trajectories.items():
+            if v[-1] == v[-2]:
+                stuck = True
+                break
+
+        if (
+            any([r == 1 for r in rewards.values()])
+            or any(agents.idle(i) for i in range(self.number_of_agents))
+            or stuck
         ):
+            if any([r == 1 for r in rewards.values()]):
+                print("Re-planning due to found target")
+            if any(agents.idle(i) for i in range(self.number_of_agents)):
+                print("Re-planning due to agent idle")
+            if stuck:
+                print("Re-planning due to stuck")
+
             # Re-plan when a target is found
             found_targets_agents = [k for k, v in rewards.items() if v == 1]
             found_targets_locations = [
